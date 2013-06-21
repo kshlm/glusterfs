575fbe6e121536f198684ad1af98672048a4a339
Kaushal M <kaushal@redhat.com>
2013-06-21 15:43:23 +0530

    Add zmq_trans_{connect,submit_{request,reply}}
    Change-Id: If6b4c88af0a870dc4a996c2da860643d21aeffe9

--- rpc/rpc-transport/zmq-trans/src/zmq-trans.c
+++ rpc/rpc-transport/zmq-trans/src/zmq-trans.c
@@ -8,13 +8,142 @@
 #define GF_DEFAULT_ZMQ_LISTEN_PORT  GF_DEFAULT_BASE_PORT
 
 int32_t
+zmq_trans_submit (rpc_transport_t *this, rpc_transport_msg_t *msg)
+{
+        int             ret = -1;
+        zmq_private_t   *priv = NULL;
+        zmq_msg_t       rpchdr;
+        zmq_msg_t       proghdr;
+        zmq_msg_t       progpayload;
+
+        GF_VALIDATE_OR_GOTO ("zmq", this, out);
+        GF_VALIDATE_OR_GOTO ("zmq", this->private, out);
+        GF_VALIDATE_OR_GOTO ("zmq", msg, out);
+
+        priv = this->private;
+        GF_VALIDATE_OR_GOTO ("zmq", priv->zmq_sock, out);
+
+
+        /* An RPC request consists of three parts - rpc header, program header &
+         * program payload. ZMQs multipart support makes it easy to send each of
+         * these as a seperate message and combine them on reception. If a part
+         * is not present in the request, we send empty messages.
+         */
+
+        if (msg->rpchdr != NULL) {
+                ret = zmq_msg_init_size (&rpchdr, sizeof (struct iovec) *
+                                         msg->rpchdrcount);
+                if (ret) {
+                        gf_log (this->name, GF_LOG_ERROR, "Failed to init "
+                                "rpchdr zmq message (%s)", strerror (errno));
+                        goto out;
+                }
+                memcpy (zmq_msg_data (&rpchdr), msg->rpchdr,
+                        sizeof (struct iovec) * msg->rpchdrcount);
+        } else {
+                ret = zmq_msg_init (&rpchdr);
+                if (ret) {
+                        gf_log (this->name, GF_LOG_ERROR, "Failed to init empty"
+                                "rpchdr zmq message (%s)", strerror (errno));
+                        goto out;
+                }
+        }
+
+        if (msg->proghdr != NULL) {
+                ret = zmq_msg_init_size (&proghdr, sizeof (struct iovec) *
+                                         msg->proghdrcount);
+                if (ret) {
+                        gf_log (this->name, GF_LOG_ERROR, "Failed to init "
+                                "proghdr zmq message (%s)", strerror (errno));
+                        goto out;
+                }
+                memcpy (zmq_msg_data (&proghdr), msg->proghdr,
+                        sizeof (struct iovec) * msg->proghdrcount);
+        } else {
+                ret = zmq_msg_init (&proghdr);
+                if (ret) {
+                        gf_log (this->name, GF_LOG_ERROR, "Failed to init empty"
+                                "proghdr zmq message (%s)", strerror (errno));
+                        goto out;
+                }
+        }
+
+        if (msg->progpayload != NULL) {
+                ret = zmq_msg_init_size (&progpayload, sizeof (struct iovec) *
+                                         msg->progpayloadcount);
+                if (ret) {
+                        gf_log (this->name, GF_LOG_ERROR, "Failed to init "
+                                "progpayload zmq message (%s)",
+                                strerror (errno));
+                        goto out;
+                }
+                memcpy (zmq_msg_data (&progpayload), msg->progpayload,
+                        sizeof (struct iovec) * msg->progpayloadcount);
+        } else {
+                ret = zmq_msg_init (&progpayload);
+                if (ret) {
+                        gf_log (this->name, GF_LOG_ERROR, "Failed to init empty"
+                                "progpayload zmq message (%s)",
+                                strerror (errno));
+                        goto out;
+                }
+        }
+
+        ret = zmq_msg_send (&rpchdr, priv->zmq_sock, ZMQ_SNDMORE);
+        if (ret) {
+                gf_log (this->name, GF_LOG_ERROR, "Failed to send rpchdr zmq "
+                        "message (%s)", strerror (errno));
+                goto out;
+        }
+
+        ret = zmq_msg_send (&proghdr, priv->zmq_sock, ZMQ_SNDMORE);
+        if (ret) {
+                gf_log (this->name, GF_LOG_ERROR, "Failed to send proghdr zmq "
+                        "message (%s)", strerror (errno));
+                goto out;
+        }
+
+        ret = zmq_msg_send (&progpayload, priv->zmq_sock, ZMQ_SNDMORE);
+        if (ret) {
+                gf_log (this->name, GF_LOG_ERROR, "Failed to send progpayload "
+                        "zmq message (%s)", strerror (errno));
+        }
+
+out:
+        zmq_msg_close (&rpchdr);
+        zmq_msg_close (&proghdr);
+        zmq_msg_close (&progpayload);
+
+        return ret;
+}
+
+int32_t
 zmq_trans_submit_request (rpc_transport_t *this, rpc_transport_req_t *req)
 {
-        return 0;
+        int             ret = -1;
+
+        GF_VALIDATE_OR_GOTO ("zmq", req, out);
+
+        ret = zmq_trans_submit (this, &req->msg);
+out:
+        return ret;
 }
 
 int32_t
 zmq_trans_submit_reply (rpc_transport_t *this, rpc_transport_reply_t *reply)
+{
+        int             ret = -1;
+
+        GF_VALIDATE_OR_GOTO ("zmq", reply, out);
+
+        ret = zmq_trans_submit (this, &reply->msg);
+out:
+        return ret;
+}
+
+static int
+zmq_trans_event_handler (int fd, int idx, void *data, int poll_int,
+                         int poll_out, int poll_err)
 {
         return 0;
 }
@@ -22,7 +151,99 @@ zmq_trans_submit_reply (rpc_transport_t *this, rpc_transport_reply_t *reply)
 int32_t
 zmq_trans_connect (rpc_transport_t *this, int port)
 {
-        return 0;
+        int             ret = -1;
+        zmq_private_t   *priv = NULL;
+        uint16_t        remote_port = -1;
+        char            *remote_host = NULL;
+        char            *zmq_endpoint = NULL;
+        void            *zmq_sock = NULL;
+        int             sock_fd = -1;
+        size_t          size = sizeof (sock_fd);
+
+        GF_VALIDATE_OR_GOTO ("zmq", this, out);
+        GF_VALIDATE_OR_GOTO ("zmq", this->private, out);
+        GF_VALIDATE_OR_GOTO ("zmq", this->options, out);
+        GF_VALIDATE_OR_GOTO ("zmq", this->ctx, out);
+
+        priv = this->private;
+        GF_VALIDATE_OR_GOTO ("zmq", priv->zmq_ctx, out);
+
+        if (port > 0) {
+                remote_port = port;
+        } else {
+                ret = dict_get_uint16 (this->options, "remote-port",
+                                       &remote_port);
+                if (ret)
+                        remote_port = GF_DEFAULT_ZMQ_LISTEN_PORT;
+                if ((uint16_t)-1 == remote_port) {
+                        ret = -1;
+                        gf_log (this->name, GF_LOG_ERROR,
+                                "remote-port has invalid value in volume %s",
+                                this->name);
+                        goto out;
+                }
+        }
+
+        ret = dict_get_str (this->options, "remote-host", &remote_host);
+        if (ret) {
+                gf_log (this->name, GF_LOG_ERROR,
+                        "remote-host is not present in volume %s", this->name);
+                goto out;
+        }
+
+        ret = gf_asprintf (&zmq_endpoint, "tcp://%s:%"PRIu16, remote_host,
+                           remote_port);
+        if (ret) {
+                gf_log (this->name, GF_LOG_ERROR,
+                        "Failed to build endpoing string");
+                goto out;
+        }
+
+        zmq_sock = zmq_socket (priv->zmq_ctx, ZMQ_REQ);
+        if (!zmq_sock) {
+                ret = -1;
+                gf_log (this->name, GF_LOG_ERROR,
+                        "Failed to create zmq socket");
+                goto out;
+        }
+
+        ret = zmq_connect (zmq_sock, (const char *)zmq_endpoint);
+        if (ret) {
+                gf_log (this->name, GF_LOG_ERROR,
+                        "zmq connect attempt on %s failed (%s)", zmq_endpoint,
+                        strerror (errno));
+                goto out;
+        }
+
+        ret = zmq_getsockopt (zmq_sock, ZMQ_FD, &sock_fd, &size);
+        if (ret) {
+                gf_log (this->name, GF_LOG_ERROR,
+                        "Failed to get zmq socket fd");
+                goto out;
+        }
+
+        priv->sock_fd = sock_fd;
+        priv->zmq_sock = zmq_sock;
+
+        priv->idx = event_register (this->ctx->event_pool, priv->sock_fd,
+                                    zmq_trans_event_handler, this, 1, 1);
+        if (priv->idx == -1) {
+                ret = -1;
+                gf_log (this->name, GF_LOG_ERROR,
+                        "Failed to register zmq event handler");
+                goto out;
+        }
+
+out:
+        if (ret) {
+                priv->sock_fd = -1;
+                if (priv->zmq_sock) {
+                        zmq_close (priv->zmq_sock);
+                        priv->zmq_sock = NULL;
+                }
+        }
+        GF_FREE (zmq_endpoint);
+        return ret;
 }
 
 int32_t
@@ -33,7 +254,7 @@ zmq_trans_disconnect (rpc_transport_t *this)
 
 static int
 zmq_trans_server_event_handler (int fd, int idx, void *data, int poll_int,
-                          int poll_out, int poll_err)
+                                int poll_out, int poll_err)
 {
         int                     ret = -1;
         rpc_transport_t         *this = NULL;
